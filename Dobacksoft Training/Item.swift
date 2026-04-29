4a87cabbf4451d37d756f60c0c33399ce6d71167
hermoso92 <antoniohermoso92@gmail.com>
2026-04-29 14:53:27 +0200

    Initial Commit

---
+++ Dobacksoft Training/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Dobacksoft Training
+//
+//  Created by Antonio Hermoso on 29/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Dobacksoft TrainingTests/Dobacksoft_TrainingTests.swift
@@ -0,0 +1,18 @@
+//
+//  Dobacksoft_TrainingTests.swift
+//  Dobacksoft TrainingTests
+//
+//  Created by Antonio Hermoso on 29/4/26.
+//
+
+import Testing
+
+struct Dobacksoft_TrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
